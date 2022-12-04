26618c32674f529a3011987d6223151b86edf3e0
Hang Wang <hang@onlyalt.com>
2022-12-04 09:29:14 -0500

    refactor day01

---
+++ src/day_01.rs
@@ -0,0 +1,37 @@
+use std::io::{BufRead, BufReader, Lines, Error};
+use std::str::FromStr;
+
+
+pub fn part_1(lines: &Vec<String>) -> i32 {
+    let mut maximum_calories = 0;
+    let mut calories = 0;
+    for line in lines {
+        if line == "" {
+            if calories > maximum_calories {
+                maximum_calories = calories;
+            }
+            calories = 0;
+        }
+        else {
+            calories += i32::from_str(line).unwrap();
+        }
+    }
+    maximum_calories
+}
+
+pub fn part_2(lines: &Vec<String>) -> i32 {
+    let mut calories_collection = vec![];
+    let mut calories = 0;
+    for line in lines {
+        if line == "" {
+            calories_collection.push(calories);
+            calories = 0;
+        }
+        else {
+            calories += i32::from_str(line).unwrap();
+        }
+    }
+    calories_collection.sort();
+    calories_collection.reverse();
+    calories_collection.into_iter().take(3).sum()
+}
---
+++ src/utils.rs
@@ -0,0 +1,7 @@
+use std::io::{BufRead, BufReader, Error};
+use std::fs::File;
+use std::path::Path;
+
+pub fn read_lines<P>(path: P) -> Result<Vec<String>, Error> where P: AsRef<Path> {
+    BufReader::new(File::open(path)?).lines().collect()
+}
