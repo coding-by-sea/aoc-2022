437d1ec47275d5d0dbc51b57def10022e5c16f2e
Hang Wang <hang@onlyalt.com>
2022-12-04 19:24:36 -0500

    finish day3

--- benches/main.rs
+++ benches/main.rs
@@ -2,9 +2,11 @@ use criterion::criterion_main;
 
 mod day_01;
 mod day_02;
+mod day_03;
 // ${BENCH_IMPORT_MARKER}
 
 criterion_main! {
     day_01::benches,
     day_02::benches,
+    day_03::benches,
 }
--- src/day_03.rs
+++ src/day_03.rs
@@ -4,8 +4,6 @@ use anyhow::{anyhow, Result, Error};
 #[derive(Debug, PartialEq)]
 struct Bitmask(u64);
 
-
-
 impl Bitmask {
     fn new(num: u8)-> Result<Self> {
         let mut num_shifts;
@@ -35,6 +33,13 @@ impl Bitmask {
             0
         }
     }
+    fn find_common_item(bitmasks: &mut Vec<Bitmask>) -> u32 {
+        let mut res = Bitmask(u64::MAX);
+        while let Some(Bitmask(num)) = bitmasks.pop() {
+            res.0 = res.0 & num;
+        }
+        res.0.trailing_zeros() + 1
+    }
 }
 
 pub fn part_1(lines: &Vec<String>) -> u32 {
@@ -59,16 +64,18 @@ pub fn part_1(lines: &Vec<String>) -> u32 {
     priorities
 }
 pub fn part_2(lines: &Vec<String>) -> u32 {
-    0
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn bitmask_parser_works() {
-        let str = "A";
-        assert_eq!(Bitmask::from_str(str).unwrap(), Bitmask((1 as u64) << 26));
+    let mut priorities = 0;
+    let mut bitmasks = vec![];
+    for line in lines {
+        let mut bitmask = Bitmask(0);
+        let bytes = line.as_bytes();
+        for i in 0..bytes.len() {
+            bitmask.add_bit(&Bitmask::new(bytes[i]).unwrap())
+            }
+        bitmasks.push(bitmask);
+        if bitmasks.len() == 3 {
+            priorities += Bitmask::find_common_item(&mut bitmasks);
+        }
     }
+    priorities
 }
